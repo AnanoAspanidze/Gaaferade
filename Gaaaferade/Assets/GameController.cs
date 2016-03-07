ad95fe1b8e9b06c247651bf83037f2ea2d0d21a1
AnanoAspanidze <anano.aspanidze@gmail.com>
2016-03-08 01:22:51 +0400

    Project Birth

---
+++ Gaaaferade/Assets/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	
+    public List<GameObject> ito;
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+	
+	}
+}
---
+++ Gaaaferade/Assets/itoScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itoScript : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+
+
+    void OnMouseDown()
+    {
+        print(gameObject.name);
+    }
+}
